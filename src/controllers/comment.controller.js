db251208df43d68e8ada3262bc05d2d320d925d8
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-02-15 19:09:05 +0500

    testing own backend

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -18,7 +18,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
     where: { videoId },
     skip: start,
     take: parseInt(limit),
-    include:{owner:true,replies:true}
+    include:{owner:true,replies:{include:{owner:true}}}
   });
   return res
     .status(httpCodes.ok)
