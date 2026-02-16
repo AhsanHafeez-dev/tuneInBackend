e22821d89fed637ca5dbf918d1badc80acd5c111
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-02-16 18:21:03 +0500

    testing own backend

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -9,6 +9,7 @@ import {
   searchQuery,
   togglePublishStatus,
   updateVideo,
+  getUploadSignature
 } from "../controllers/video.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
